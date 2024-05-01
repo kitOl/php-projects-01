0d63365d4bd718b88ae32a3272f5bdf3b167db7f
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-05-02 00:04:01 +0300

    3 add signup error messages codes

--- 03/signup.php
+++ 03/signup.php
@@ -17,6 +17,39 @@ include_once 'header.php';
     </form>
   </div>
 
+  <?php
+  if (isset($_GET['error'])) :
+    if ($_GET['error'] == 'emptyinput') : ?>
+
+      <p class="error">Fill in all fields!</p>
+
+    <?php elseif ($_GET['error'] == 'invaliduid') : ?>
+
+      <p class="error">Choose a proper username!</p>
+
+    <?php elseif ($_GET['error'] == 'invalidemail') : ?>
+
+      <p class="error">Choose a proper email!</p>
+
+    <?php elseif ($_GET['error'] == 'pwdsnotmatch') : ?>
+
+      <p class="error">Passwords doesn't match!</p>
+
+    <?php elseif ($_GET['error'] == 'stmtfailed') : ?>
+
+      <p class="error">Something went wrong, try again!</p>
+
+    <?php elseif ($_GET['error'] == 'usernametaken') : ?>
+
+      <p class="error">Username already taken!</p>
+
+    <?php elseif ($_GET['error'] == 'none') : ?>
+
+      <p class="success">You have signed up!</p>
+
+  <?php endif;
+  endif; ?>
+
 </section>
 
 <?php
