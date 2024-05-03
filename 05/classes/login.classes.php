0fa2c4bd1e8969a7bde786e78529af7bd37d6b04
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-05-03 17:43:57 +0300

    5 fixed login class code

--- 05/classes/login.classes.php
+++ 05/classes/login.classes.php
@@ -4,7 +4,7 @@ class Login extends Dbh
 {
   protected function getUser($uid, $pwd)
   {
-    $sql = "SELECT pwd FROM users WHERE uid = ? OR email = ?;";
+    $sql = "SELECT * FROM users WHERE uid = ? OR email = ?;";
     $stmt = $this->connect()->prepare($sql);
 
     if (!$stmt->execute([$uid, $uid])) {
@@ -13,36 +13,21 @@ class Login extends Dbh
       exit();
     }
 
-    if ($stmt->rowCount() == 0) {
+    $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if (count($user) == 0) {
       $stmt = null;
       header("Location: ../index.php?error=usernotfound");
       exit();
     }
 
-    $pwdHashed = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $pwdHashed = $user[0]['pwd'];
 
-    if (!password_verify($pwd, $pwdHashed[0]['pwd'])) {
+    if (!password_verify($pwd, $pwdHashed)) {
       $stmt = null;
       header("Location: ../index.php?error=wrongpassword");
       exit();
     }
-
-    $sql = "SELECT * FROM users WHERE uid = ? OR email = ? AND pwd = ?;";
-    $stmt = $this->connect()->prepare($sql);
-
-    if (!$stmt->execute([$uid, $uid, $pwd])) {
-      $stmt = null;
-      header("Location: ../index.php?error=stmtfailed");
-      exit();
-    }
-
-    if ($stmt->rowCount() == 0) {
-      $stmt = null;
-      header("Location: ../index.php?error=usernotfound");
-      exit();
-    }
-
-    $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     session_start();
 
